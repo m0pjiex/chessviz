e8019332d522fda3839c03200a7a94d898d7bef0
m0pjiex <trolologhost3@gmail.com>
2018-05-06 21:49:01 +0700

    Created a directory for files

---
+++ src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "board_read.h"
+#include "board_print_plain.h"
+#include "board.h"
+int main() {
+char  a[9][9];
+int y=1;
+bbuild(a);
+bprint(a);
+do{
+printf("\nВаш ход?\n");
+step(a);
+bprint(a);
+printf("\nПродолжить игру? 1- да, 0 - нет\n");
+scanf("%d",&y);
+}while(y);
+return 0;
+}
