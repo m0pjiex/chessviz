719241ef8ecc7c17dcac1539b0795ad12750e991
m0pjiex <trolologhost3@gmail.com>
2018-05-14 03:12:02 +0700

    Fixed boardPrintPlain

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include "board_print_plain.h"
 void bPrint ( char num[9][9]) {
-    
-    for (int o = 0; o < 9; o++){
-	for (int p = 0; p < 9; p++){
+    int o;
+int p;
+    for (o = 0; o < 9; o++){
+	for (p = 0; p < 9; p++){
 	    printf("%c ",num[o][p]);
 	}
 	printf("\n");
